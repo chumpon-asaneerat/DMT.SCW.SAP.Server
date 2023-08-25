808adbcf9d797a2d053b51e9f25fc86a02cfcdac
chumpon-asaneerat <chumponsenate@gmail.com>
2023-08-25 10:43:00 +0700

    - Init project #8

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 const express = require('express')
+const basicAuth = require('express-basic-auth')
 const https = require('https')
 const path = require('path')
 const fs = require('fs')
 
 const app = express()
+app.use(basicAuth({
+    users: { 'api_user': 'DMTinf#2023' },
+    unauthorizedResponse: getUnauthorizedResponse
+}))
+
+function getUnauthorizedResponse(req, res) {
+    return {
+        msg: req.auth ? ('Credentials ' + req.auth.user + ':' + req.auth.password + ' rejected') : 'No credentials provided'
+    }
+    
+}
 
 app.use('/', (req, res, next) => { 
     res.send('Hello from SSL server')
 })
 
-const sslServer = https.createServer(
-    {
+const sslServer = https.createServer({
         key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
         cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
-    }, 
-    app
-)
+    }, app)
 
 sslServer.listen(3443, () => { 
     console.log('secure server on port 3443')
