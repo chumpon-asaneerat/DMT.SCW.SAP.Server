d449936ab8744665d181ac586054ffc5a56d1a67
chumpon-asaneerat <chumponsenate@gmail.com>
2023-09-12 19:59:22 +0700

    - Init project #10

--- server.js
+++ server.js
@@ -1,14 +1,42 @@
 const express = require('express')
 const basicAuth = require('express-basic-auth')
 const https = require('https')
-const path = require('path')
-const fs = require('fs')
 
+const path = require('path');
+//const find = require('find');
+const fs = require('fs');
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cookieparser = require("cookie-parser");
+const bodyparser = require("body-parser");
+const compression = require("compression");
+
+// Create express app.
 const app = express()
+
+const rootPath = process.env['ROOT_PATHS'];
+
+// ##--------------------
+// ## init middlewares
+// ##--------------------
+// Init Basic Auth
 app.use(basicAuth({
     users: { 'api_user': 'DMTinf#2023' },
     unauthorizedResponse: getUnauthorizedResponse
 }))
+// Init Helmet
+app.use(helmet());
+// Init Logger
+app.enable("trust proxy");
+app.use(morgan("short", { stream: logger.stream }));
+// Init Cookie Parser
+let secret = 'YOUR_SECURE_KEY@123'
+app.use(cookieparser(secret));
+// Init Body Parser
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+// Init Compression
+app.use(compression())
 
 function getUnauthorizedResponse(req, res) {
     return {
@@ -22,8 +50,8 @@ app.use('/', (req, res, next) => {
 })
 
 const sslServer = https.createServer({
-        key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-        cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+        key: fs.readFileSync(path.join(rootPath, 'cert', 'key.pem')),
+        cert: fs.readFileSync(path.join(rootPath, 'cert', 'cert.pem'))
     }, app)
 
 sslServer.listen(3443, () => { 
