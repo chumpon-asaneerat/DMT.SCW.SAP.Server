08fa16f4be70b9a6bc8bf71d80ebcd5f39569187
chumpon-asaneerat <chumponsenate@gmail.com>
2023-09-14 10:15:20 +0700

    - Update https route jsonfile #3

--- routes/api/JsonFiles/routes.js
+++ routes/api/JsonFiles/routes.js
@@ -8,12 +8,20 @@ const rootPath = process.env['ROOT_PATHS'];
 // init logger
 const logger = require(path.join(rootPath, 'lib', 'logger')).logger
 
+const jsonfile = require(path.join(rootPath, 'lib', 'jsonfile'))
+const moment = require('moment')
+
 //#endregion
 
-const execute = (req, res, next) => {
-    //res.send('Hello from SSL server')
+const saveJsonFile = (req, res, next) => {
+    let fname = 'req.' + moment().format('YYYY.MM.DD.HH.mm.ss.SSSS') + '.json'
+    let filename = path.join(rootPath, 'jsonfiles', fname)
+    let value = req.body
+    jsonfile.save(filename, value)
+
     let obj = {
-        value: 'not implements.'
+        value: 'not implements.',
+        headers: req.headers
     }
     res.json(obj)
 
@@ -21,7 +29,7 @@ const execute = (req, res, next) => {
 }
 
 const init_routes = (app) => {
-    app.post('/api/secure/json', execute);
+    app.post('/api/secure/json/save', saveJsonFile);
 };
 
 module.exports.init_routes = exports.init_routes = init_routes;
