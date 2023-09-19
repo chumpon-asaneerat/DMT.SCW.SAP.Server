78ae1b9374c1ce033810b6b776544a86f858f2b0
chumpon-asaneerat <chumponsenate@gmail.com>
2023-09-19 18:51:49 +0700

    - Implements db access #4

--- TAxTOD.db.js
+++ TAxTOD.db.js
@@ -27,6 +27,52 @@ const TAxTOD = class extends SqlServer {
     }
 
     //#endregion
+
+    //#region TestSP
+
+    async SaveCouponReservation(pObj) {
+        let name = 'SaveCouponReservation'        
+        let inputs = [
+            { "name": "postingdate", "type": "char(8)" },
+            { "name": "mat_slip", "type": "char(16)" },
+            { "name": "headertext", "type": "char(25)" },
+            { "name": "itemnumber", "type": "char(4)" },
+            { "name": "materialnum", "type": "char(10)" },
+            { "name": "quantity", "type": "int" },
+            { "name": "unit", "type": "char(4)" },
+            { "name": "plant", "type": "char(4)" },
+            { "name": "location", "type": "char(4)" },
+            { "name": "goodsrecipient", "type": "char(12)" },
+            { "name": "matdescription", "type": "char(40)" }
+        ]
+        let outputs = [
+            { "name": "errNum", "type": "int" },
+            { "name": "errMsg", "type": "nvarchar(max)" }
+        ]
+        return await this.execute(name, pObj, inputs, outputs);
+    }
+
+    //#endregion
+
+    //#region SaveReceivedCoupon
+
+    async SaveReceivedCoupon(pObj) {
+        let name = 'SaveReceivedCoupon'
+        let inputs = [
+            { "name": "materialnum", "type": "nvarchar(10)" },
+            { "name": "location", "type": "nvarchar(5)" },
+            { "name": "SerialNo", "type": "nvarchar(7)" },
+            { "name": "mat_slip", "type": "nvarchar(12)" },
+            { "name": "matdescription", "type": "nvarchar(20)" }
+        ]
+        let outputs = [
+            { "name": "errNum", "type": "int" },
+            { "name": "errMsg", "type": "nvarchar(max)" }
+        ]
+        return await this.execute(name, pObj, inputs, outputs);
+    }
+
+    //#endregion
 }
 
 module.exports = exports = TAxTOD;
