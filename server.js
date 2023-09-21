1ff74974b3550a010634a97f1e8bcf4eee5d6df2
chumpon-asaneerat <chumponsenate@gmail.com>
2023-09-21 13:57:15 +0700

    - Implements schedule #1

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const rootPath = process.env['ROOT_PATHS'];
 const logger = require(path.join(rootPath, 'lib', 'logger')).logger
 
 const express = require('express')
-const https = require('https')
 
 const middlewares = require(path.join(rootPath, 'lib', 'middlewares'))
 const routeManager = require(path.join(rootPath, 'lib', 'route-manager'))
@@ -29,15 +28,33 @@ const app = express()
 middlewares.init_middlewares(app)
 routeManager.init_routes(app)
 
-// create http server
+// create https server
+/*
+const https = require('https')
+
 const sslServer = https.createServer({
         key: fs.readFileSync(path.join(rootPath, 'cert', 'key.pem')),
         cert: fs.readFileSync(path.join(rootPath, 'cert', 'cert.pem'))
     }, app)
-
 
 sslServer.listen(port, () => { 
     let msg = `${appname} listen on port: ${port}`
     console.log(msg)
     logger.info(msg);
 })
+*/
+
+// create http server
+const http = require('http')
+
+const webServer = http.Server(app)
+
+webServer.listen(port, () => { 
+    let msg = `${appname} listen on port: ${port}`
+    console.log(msg)
+    logger.info(msg);
+})
+
+setInterval(() => {
+    
+}, 1000)
