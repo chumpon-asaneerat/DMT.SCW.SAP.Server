0ca265d6c29d01454b2d9b25ae2b7bc6a3c191b3
chumpon-asaneerat <chumponsenate@gmail.com>
2023-09-21 14:35:40 +0700

    - Implements schedule #2

--- server.js
+++ server.js
@@ -55,6 +55,41 @@ webServer.listen(port, () => {
     logger.info(msg);
 })
 
-setInterval(() => {
-    
-}, 1000)
+
+const schedule = require('node-schedule')
+
+// The cron format consists of:
+//
+// *    *    *    *    *    *
+// ┬    ┬    ┬    ┬    ┬    ┬
+// │    │    │    │    │    │
+// │    │    │    │    │    └ day of week (0 - 7) (0 or 7 is Sun)
+// │    │    │    │    └───── month (1 - 12)
+// │    │    │    └────────── day of month (1 - 31)
+// │    │    └─────────────── hour (0 - 23)
+// │    └──────────────────── minute (0 - 59)
+// └───────────────────────── second (0 - 59, OPTIONAL)
+
+
+// const job = schedule.scheduleJob('*/5 * * * * *', () => {
+//     // run every 5 s.
+//     console.log('The answer to life, the universe, and everything!')
+// })
+
+
+// Use Recurrence Rule - every 2 second
+const rule = new schedule.RecurrenceRule()
+let times = []
+for (let i = 1; i < 60; i++) { 
+    if ((i % 2) == 0) times.push(i)
+}
+rule.second = times
+
+const job = schedule.scheduleJob(rule, () => {
+    console.log('The answer to life, the universe, and everything!')
+});
+
+// gracefully shutdown jobs when a system interrupt occurs.
+process.on('SIGINT', () => { 
+    schedule.gracefulShutdown().then(() => process.exit(0))
+})
